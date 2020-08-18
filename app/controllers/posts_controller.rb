47628ad69d86f3dbc4a7e1a9318e08400a87443c
king58-1224 <king581224@gmail.com>
2020-08-18 21:15:30 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index 
     @posts = Post.all.order(id: "DESC")
   end
-
+ 
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
@@ -11,13 +11,13 @@ class PostsController < ApplicationController
 
   def checked
     post = Post.find(params[:id])
-    if post.checked
+    if post.checked then
       post.update(checked: false)
     else
       post.update(checked: true)
     end
 
     item = Post.find(params[:id])
-    render json: { ppost: item }
+    render json: { post: item }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 2020_07_24_032242) do
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
     t.boolean "checked"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
